87c36742f7762d42fd71ba721f12decb7a018e87
comrade-del <pobehi18@hmail.com>
2023-10-10 19:08:20 +0100

    can't remember what I wanted to do, created voting database

--- modules/functions.py
+++ modules/functions.py
@@ -8,20 +8,21 @@ _id = None
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["myapp"]
-collection = db["data"]
-usercol = db["user"]
+census = db["data"]
+users = db["user"]
+voting = db["voting"]
 
 
 def update(_id, msg, db):  # will make separate for update
     while True:
         who = {"_id": _id}
         if db == "census":
-            x = collection.find_one(who)
+            x = census.find_one(who)
             if x is None:
                 print("You have not filled the form")
                 break
         elif db == "user":
-            x = usercol.find_one(who)
+            x = users.find_one(who)
         print(x)
         print("What would you like to edit\nType \"exit\" if you changed your mind")
         option = input(": ").lower()
@@ -31,7 +32,7 @@ def update(_id, msg, db):  # will make separate for update
             print("Okay")
             break
         else:
-            collection.find_one(who)
+            census.find_one(who)
             try:
                 assert x[option]
                 new = input(f"Enter new {option}: ")
@@ -39,9 +40,9 @@ def update(_id, msg, db):  # will make separate for update
                 _new = {"$set": {option: new}}
                 x[option] = new
                 if db == "census":
-                    collection.update_one(_old, _new)
+                    census.update_one(_old, _new)
                 elif db == "user":
-                    usercol.update_one(_old, _new)
+                    users.update_one(_old, _new)
                 print(msg)
                 break
             except KeyError:
@@ -67,13 +68,13 @@ def create(x, msg, db):
         really = input("Is this correct? (Y/N): ")
         if really == "Y".lower():
             if db == "user".lower():
-                u = usercol.insert_one(x)
+                u = users.insert_one(x)
                 user_id = u.inserted_id
                 print(msg)
                 # print(f'Your ID is {user_id}')  # works, just need to work out custom IDs in my head
             elif db == "census".lower():
                 try:
-                    c = collection.insert_one(x)
+                    c = census.insert_one(x)
                     print(msg)
                 except DuplicateKeyError:
                     print("You already have an entry. Maybe you mean update?")
@@ -89,19 +90,11 @@ def create(x, msg, db):
             print("Invalid option")
 
 
-"""try:
-        # Attempt to insert a new user document
-        collection.insert_one({"user_name": user_name, "password": password})
-        print("User created successfully.")
-    except DuplicateKeyError:
-        print("Username already exists. Please choose a different username.")"""
-
-
 def login():
     user = input("Name?: ")
     password = input("Password?: ")
     who = {"username": user, "password": password}
-    authenticated_user = usercol.find_one(who)
+    authenticated_user = users.find_one(who)
     if authenticated_user:
         privilege = authenticated_user.get("type")
         return authenticated_user["_id"], privilege  # Return the user_id and privilege of the authenticated user
