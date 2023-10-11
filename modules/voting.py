4409fbaf99d1ead39583c3003638b9bc03f9565c
comrade-del <pobehi18@hmail.com>
2023-10-11 20:13:49 +0100

    unmotivated

--- modules/voting.py
+++ modules/voting.py
@@ -1,12 +1,33 @@
 # Election
 
-from modules.functions import create
+from modules.functions import create, update
 
 
 def voting(user_id, privilege):
     print("Election Time")
-    candidate = dict(name=input("Candidate name: "), party=input("Represented party: "),
-                     gender=input("Gender: "), vote_count=0)
-
-    create(candidate, msg="Candidate added successfully")
-    # turn into if else for adding candidates and voting or make into functions
+    print("You must be logged in as a user to vote")
+    while True:
+        if privilege == "user":
+            _do = eval(input("What would you like to do?\n1: Vote\n2: View results\n3: Exit: "))
+            if _do == 1:
+                pass  # increase vote_count function
+            elif _do == 2:
+                pass  # fetch details
+            elif _do == 3:
+                break
+            else:
+                print("Invalid option. Type either \"1\", \"2\" or \"3\"")
+        elif privilege == "admin":
+            _do = eval(input("What would you like to do?\n1: Add new Candidate\n2: Update candidate details\n3: Delete Candidate\n4: Exit: "))
+            if _do == 1:
+                candidate = dict(name=input("Candidate name: "), party=input("Represented party: "),
+                                 gender=input("Gender: "), vote_count=0)
+                create(candidate, msg="Candidate added successfully", db="voting")
+            elif _do == 2:
+                update(_id=user_id, msg="Details edited Successfully", db="voting")
+            elif _do == 3:
+                pass  # delete function
+            elif _do == 4:
+                break
+            else:
+                print("Invalid option. Type either \"1\", \"2\" or \"3\"")
